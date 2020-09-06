621330e6fe90d340eb774187928327f313a3e239
adfa0117 <37145856+adfa0117@users.noreply.github.com>
2020-09-06 23:18:44 +0900

    Rhythm Typing Start

---
+++ Unity/Rhythm Typing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject prefab;
+
+    void Start()
+    {
+        StartCoroutine("CreateNote");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Detected key code: " + Event.current);
+    }
+
+    IEnumerator CreateNote() {
+        while (true) {
+            float x = Random.Range(-8.5f, 8.5f);
+            float y = Random.Range(-4.5f, 4.5f);
+            Instantiate(prefab, new Vector3(x, y, 0), prefab.transform.rotation);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Unity/Rhythm Typing/Assets/Scripts/typingNote.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class typingNote : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    static int recentNoteId;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = 0.1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
